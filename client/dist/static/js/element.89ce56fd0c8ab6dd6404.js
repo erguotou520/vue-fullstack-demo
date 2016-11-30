818cf1bf4fa727127f828b49a857027c89f64a02
erguotou <erguotou525@gmail.com>
2016-11-30 18:02:31 +0800

    reverse dist folder

---
+++ client/dist/static/js/element.89ce56fd0c8ab6dd6404.js
@@ -0,0 +1,2 @@
+webpackJsonp([9,6,8],[]);
+//# sourceMappingURL=element.89ce56fd0c8ab6dd6404.js.map
---
+++ client/dist/static/js/vendor.63ab17f50de3680556bd.js
@@ -0,0 +1,2 @@
+webpackJsonp([8,6,9],[]);
+//# sourceMappingURL=vendor.63ab17f50de3680556bd.js.map

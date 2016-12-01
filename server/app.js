25125b2ef98dfa7662425af21aaf628b19f1a32b
erguotou <erguotou525@gmail.com>
2016-12-01 13:52:57 +0800

    check node-start

--- server/app.js
+++ server/app.js
@@ -2,33 +2,52 @@
  * Main application file
  */
 
-'use strict'
+// 'use strict'
+//
+// // Set default node environment to development
+// process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+//
+// var express = require('express')
+// var mongoose = require('mongoose')
+// var config = require('../config').backend
+//
+// // Connect to database
+// mongoose.connect(config.mongo.uri, config.mongo.options)
+//
+// // insure DB with admin user data
+// require('./config/seed')
+//
+// // Setup server
+// var app = express()
+// var server = require('http').createServer(app)
+// var socketio = require('socket.io')(server)
+// require('./config/socketio')(socketio)
+// require('./config/express')(app)
+// require('./routes')(app)
+//
+// // Start server
+// server.listen(config.port, function () {
+//   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'))
+// })
+//
+// // Expose app
+// exports = module.exports = app
+//
+var express = require('express');
+var app = express();
 
-// Set default node environment to development
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+app.set('port', (process.env.PORT || 5000));
 
-var express = require('express')
-var mongoose = require('mongoose')
-var config = require('../config').backend
+app.use(express.static(__dirname + '/public'));
 
-// Connect to database
-mongoose.connect(config.mongo.uri, config.mongo.options)
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-// insure DB with admin user data
-require('./config/seed')
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
 
-// Setup server
-var app = express()
-var server = require('http').createServer(app)
-var socketio = require('socket.io')(server)
-require('./config/socketio')(socketio)
-require('./config/express')(app)
-require('./routes')(app)
-
-// Start server
-server.listen(config.port, function () {
-  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'))
-})
-
-// Expose app
-exports = module.exports = app
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});

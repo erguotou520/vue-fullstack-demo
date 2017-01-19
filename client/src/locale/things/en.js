7d96d0ca6bc7489bc9dc1fdc7aa02fed7222ac98
erguotou <erguotou525@gmail.com>
2017-01-19 17:56:28 +0800

    update readme

---
+++ client/src/locale/things/en.js
@@ -0,0 +1,17 @@
+export default {
+  breadcrumb: {
+    home: 'Home',
+    current: 'Things'
+  },
+  model: {
+    name: 'name',
+    description: 'description'
+  },
+  rules: {
+    name: 'Please input the name'
+  },
+  edit: {
+    create: 'Add thing',
+    update: 'Update thing'
+  }
+}
